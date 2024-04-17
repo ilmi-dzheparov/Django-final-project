dd596dcb55975c30e4a059f1f2df03823fe57b9d
Максим Ландау <12landay@mail.ru>
2024-04-17 12:15:40 +0300

    stage1/create models for product and category/fix

--- src/products/models.py
+++ src/products/models.py
@@ -36,9 +36,6 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
     preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path)
-    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    sellers_count = models.PositiveIntegerField()
-    reviews_count = models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
