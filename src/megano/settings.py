89e50b10050410dec9638acbc1ce103f612526d8
danila_goncharov <danniila2000@mail.ru>
2024-04-17 18:43:57 +0300

    stage1/catalog-category-menu/fiture настроено кэширование

--- src/megano/settings.py
+++ src/megano/settings.py
@@ -110,6 +110,14 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        'LOCATION': "/var/tmp/django_cache",
+    },
+}
+
+
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from models import Category, Product
+from django.core.cache import cache
 
 
 class ProductListView(ListView):
@@ -8,6 +9,15 @@ class ProductListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
+        cache_key = 'categories'
+        categories = cache.get(cache_key)
+        if categories is None:
+            categories = Category.objects.all()
+            context[cache_key] = categories
+            cache.set(cache_key, categories, 86400)
+        else:
+            context[cache_key] = categories
+
         context['products'] = Product.objects.all()
-        context['categories'] = Category.objects.all()
+
         return context
