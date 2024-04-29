3e596dd17186530070adedcd677b27423d129be4
vladislav_finogenov <Finogenov.V.A@yandex.ru>
2024-04-29 23:07:52 +0300

    Добавлен маршрут для создания отзыва

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ProductListView
+from .views import (
+    ProductListView,
+    ReviewCreateView,
+)
 
 app_name = "shop"
 
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="shop"),
+    path('product/<int:pk>/review/create/', ReviewCreateView.as_view(), name='review_create'),
     ]
