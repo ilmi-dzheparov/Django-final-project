20a8a2ea9470dc69c3ab5bdfeeb5ece87cc4d4de
Alorian <slowpok19450509@gmail.com>
2024-05-08 00:54:47 +0200

    stage2/пользователь может указывать только себя в качестве продавца/feat

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -17,8 +17,21 @@ class SellerProductAdmin(admin.ModelAdmin):
     list_display_links = ["product"]
 
     def get_queryset(self, request):
+        """
+        Продавец может видеть только свои SellerProduct
+        """
         queryset = super().get_queryset(request)
         if request.user.is_superuser:
             return queryset
         else:
-            return queryset.filter(seller__email=request.user.email)
+            return queryset.filter(seller__user=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        При создании SellerProduct, пользователь может указать только себя в выпадающем списке seller
+        """
+        if db_field.name == "seller":
+            seller = Seller.objects.get(user=request.user)
+            kwargs["queryset"] = Seller.objects.filter(pk=seller.pk)
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
