e28c800161e7d4b486006a4cae98bef5211c3f23
Alorian <slowpok19450509@gmail.com>
2024-05-07 22:14:39 +0200

    stage2/модель продавца и его товара добавлены в админ-панель/feat

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category, Seller, SellerProduct
 
 admin.site.register(Product)
+admin.site.register(Category)
+
+
+@admin.register(Seller)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "email", "phone", "address"]
+    list_display_links = ["pk", "name"]
+
+
+@admin.register(SellerProduct)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "product", "price", "quantity"]
+    list_display_links = ["product"]
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        else:
+            return queryset.filter(seller__email=request.user.email)
