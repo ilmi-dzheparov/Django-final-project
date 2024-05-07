ed4ba82009b55e1a697199a98de1c0614e258b61
Maksim Semenyuk <expmax85@gmail.com>
2024-05-08 00:25:48 +0300

    fix after merge

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -5,6 +5,6 @@ app_name = "shop"
 
 
 urlpatterns = [
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail")
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
     path('product/<int:pk>/review/create/', ReviewCreateView.as_view(), name='review_create'),
     ]
