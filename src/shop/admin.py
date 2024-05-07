c24d8ae80010b0727ddc0246b0e41bed6c8616a7
Alorian <slowpok19450509@gmail.com>
2024-05-07 22:14:39 +0200

    stage2/модель продавца и его товара добавлены в админ-панель/feat

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,44 +1,24 @@
 from django.contrib import admin
-from shop.models import Product, Review, Category
+from .models import Product, Category, Seller, SellerProduct
 
 admin.site.register(Product)
 admin.site.register(Category)
 
 
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['product', 'author', 'review', 'created_at']
-    list_select_related = ['product', 'author']
-    search_fields = ('product__name',)
-    list_filter = ['created_at']
+@admin.register(Seller)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "email", "phone", "address"]
+    list_display_links = ["pk", "name"]
 
-    def has_add_permission(self, request):
-        """
-        Метод запрещает создавать комментарии в админ-панели
-        """
 
-        return False
+@admin.register(SellerProduct)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "product", "price", "quantity"]
+    list_display_links = ["product"]
 
-    def has_change_permission(self, request, obj=None):
-        """
-        Метод запрещает изменять комментарии в админ-панели
-        """
-
-        return False
-
-    def get_search_results(self, request, queryset, search_term):
-        """
-        Метод для получения результатов поиска по заданному запросу.
-        """
-
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
-        if search_term:
-            queryset |= self.model.objects.filter(product__name__icontains=search_term)
-        return queryset, use_distinct
-
-    def review(self, obj):
-        """
-        Метод ограничивает длину отображаемых символов до "100"
-        """
-
-        return (obj.text[:100] + '...') if len(obj.text) > 100 else obj.text
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        else:
+            return queryset.filter(seller__email=request.user.email)
