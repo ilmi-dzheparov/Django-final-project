34369852142c3e1ff90f921cd9785224cf6859d9
ilmi <jilmi@yandex.ru>
2024-05-10 15:20:48 +0100

    Создание модели характеристик товаров / исправление

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,8 +1,88 @@
 from django.contrib import admin
-from shop.models import Product, Review, Category
+from shop.models import (
+    Product,
+    Review,
+    Category,
+    Attribute,
+    ProductAttribute
+)
+from .forms import AttributeFormSet, ProductAttributeFormSet, CustomAttributeAdminForm
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class AttributesInLine(admin.TabularInline):
+    """
+    Инлайн-форма создания attributes (характеристик), которая буде добавлена в админ панель модели Category
+    """
+
+    model = Attribute
+    formset = AttributeFormSet
+    extra = 1
+
+
+class ProductAttributeInline(admin.TabularInline):
+    """
+    Создание инлайновой формы добавления характеристик в административной панели Django.
+    Родительская модель Product
+    """
+    model = ProductAttribute
+    formset = ProductAttributeFormSet
+    extra = 1
+
+    """
+    Настройки виджета полей ForeignKey attribute (характеристика) в административной панели Djangoю
+    Цель: при выборе attribute доступны только attriutes (характреристики), которые связаны с категорией товара
+    """
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "attribute":
+            # Получаем категорию товара, для которого создается или редактируется атрибут
+            product_id = request.resolver_match.kwargs.get('object_id')
+            if product_id:
+                product = Product.objects.get(pk=product_id)
+                product_category = product.category
+                # Фильтруем характеристики по категории товара
+                kwargs["queryset"] = Attribute.objects.filter(category=product_category)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductAttributeInline]
+    """
+       Метод get_formsets_with_inlines определяет какие формы и inline-формы будут отображаться 
+       на странице изменения объекта в административной панели Django. 
+       Переопределяем метод, чтоб инлайн-форма отображалась если редактируем объект.
+       При создании объекта форма не будет отображаться
+       """
+
+    def get_formsets_with_inlines(self, request, obj=None):
+        if obj:
+            return super().get_formsets_with_inlines(request, obj)
+        return []
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [AttributesInLine]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ['category', 'name', 'unit']
+    list_editable = ['name', 'unit']
+    list_filter = ['category', ]
+    fieldsets = [
+        ('Категория', {
+            'fields': ('category',)
+        }),
+        ('Характеристика', {
+            'fields': ('name', 'unit')
+        }),
+    ]
+
+    form = CustomAttributeAdminForm
 
 
 @admin.register(Review)
