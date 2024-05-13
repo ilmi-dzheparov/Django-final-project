125e194878fda09368caafb16e641e7b6f8337f4
Alorian <slowpok19450509@gmail.com>
2024-05-13 18:47:10 +0200

    stage2/SellerProduct и Product зарегистрированы в admin/feat

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -5,7 +5,9 @@ from shop.models import (
     Review,
     Category,
     Attribute,
-    ProductAttribute
+    ProductAttribute,
+    Seller,
+    SellerProduct
 )
 from .forms import AttributeFormSet, ProductAttributeFormSet, CustomAttributeAdminForm
 
@@ -123,3 +125,34 @@ class AttributeAdmin(admin.ModelAdmin):
     ]
 
     form = CustomAttributeAdminForm
+
+@admin.register(Seller)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name", "email", "phone", "address"]
+    list_display_links = ["pk", "name"]
+
+
+@admin.register(SellerProduct)
+class SellerProductAdmin(admin.ModelAdmin):
+    list_display = ["pk", "product", "price", "quantity"]
+    list_display_links = ["product"]
+
+    def get_queryset(self, request):
+        """
+        Продавец может видеть только свои SellerProduct
+        """
+        queryset = super().get_queryset(request)
+        if request.user.is_superuser:
+            return queryset
+        else:
+            return queryset.filter(seller__user=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """
+        При создании SellerProduct, пользователь может указать только себя в выпадающем списке seller
+        """
+        if db_field.name == "seller":
+            seller = Seller.objects.get(user=request.user)
+            kwargs["queryset"] = Seller.objects.filter(pk=seller.pk)
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
