7183568aa73b8c806f529c5349814e6754f95f87
Alorian <slowpok19450509@gmail.com>
2024-05-14 00:57:04 +0200

    stage2/Seller теперь связан с кастомным User'ом/feat

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 from taggit.managers import TaggableManager
 
 
@@ -8,6 +10,10 @@ def product_preview_directory_path(instance: "Product", filename: str) -> str:
         pk=instance.pk,
         filename=filename,
     )
+
+
+def seller_thumbnail_directory_path(instance: "Seller", filename: str) -> str:
+    return f"shop/seller_thumbnails/seller_{instance.pk}/{filename}"
 
 
 class Category(models.Model):
@@ -29,8 +35,6 @@ class Category(models.Model):
         return self.name
 
 
-
-
 class Product(models.Model):
     """
       Модель Product представляет товар,
@@ -40,12 +44,12 @@ class Product(models.Model):
     class Meta:
         ordering = ["name"]
         verbose_name = "product"
-        verbose_name_plural = "shop"
+        verbose_name_plural = "products"
 
     name = models.CharField(max_length=100, db_index=True)
     description = models.TextField(blank=True, null=True)
     preview = models.ImageField(null=True, blank=True, upload_to=product_preview_directory_path)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='shop')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     available = models.BooleanField(default=True)
     tags = TaggableManager(blank=True)
 
@@ -54,6 +58,40 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_details", kwargs={"pk": self.pk})
+
+
+class Review(models.Model):
+    """
+    Модель для хранения отзывов.
+    """
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='reviews_author'
+    )
+
+    text = models.TextField(
+        max_length=3000
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+
+    def __str__(self):
+        return f'Отзыв от {self.created_at}'
 
 
 class Attribute(models.Model):
@@ -92,3 +130,42 @@ class ProductAttribute(models.Model):
                                   verbose_name='характеристика',
                                   )
     value = models.CharField(max_length=250, verbose_name='значение')
+
+
+class Seller(models.Model):
+    """
+    Модель Seller представляет продавца,
+    который может размещать свои товары (SellerProduct) в магазине.
+    """
+
+    class Meta:
+        ordering = ["name"]
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="seller", on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, db_index=True, null=False)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to=seller_thumbnail_directory_path)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("seller_details", kwargs={"pk": self.pk})
+
+
+class SellerProduct(models.Model):
+    """
+    Модель SellerProduct представляет товар, который продает конкретный продавец (Seller).
+    Эта модель связана с Product и отличается ценой и количеством от продавца к продавцу.
+    """
+
+    seller = models.ForeignKey(Seller, related_name="products", on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, related_name="seller_products", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
