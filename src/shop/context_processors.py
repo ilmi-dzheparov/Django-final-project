76774de04aae77a2055b124f56a4e6202399c524
danila_goncharov <danniila2000@mail.ru>
2024-05-15 16:58:49 +0300

    stage3/catalog/update добавлено кэширование

--- src/shop/context_processors.py
+++ src/shop/context_processors.py
@@ -1,5 +1,9 @@
-from .services import get_cached_categories
+from .services import get_cached_categories, get_cached_products
 
 
 def categories(request):
     return {'categories': get_cached_categories()}
+
+
+def products(request, tag, sort):
+    return {f'products-{sort}-{tag}': get_cached_products(tag, sort)}
--- src/shop/services.py
+++ src/shop/services.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 from django.conf import settings
-from .models import Category
+from .models import Category, SellerProduct
 
 
 def get_cached_categories():
@@ -10,3 +10,27 @@ def get_cached_categories():
         categories = Category.objects.filter(products__available=True).prefetch_related('products')
         cache.set(cache_key, categories, settings.DEFAULT_CACHE_TIME)
     return categories
+
+
+def get_cached_products(tag, sort):
+    cache_key = f'products-{sort}-{tag}'
+    products = cache.get(cache_key)
+    if products is None:
+
+        products = SellerProduct.objects.all()
+
+        if sort == 'price':
+            products = products.order_by('price')
+        if sort == 'review':
+            products = products.product.order_by('-reviews')
+        if sort == 'newness':
+            products = products.order_by('created_at')
+        if sort == 'popularity:':
+            products = products.order_by('-popularity')
+
+        if tag:
+            products = products.filter(tags__name=tag)
+
+        cache.set(cache_key, products, settings.DEFAULT_CACHE_TIME)
+
+    return products
