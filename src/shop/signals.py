f484fc69c8fb290132eac4c50b7bc8b699457954
danila_goncharov <danniila2000@mail.ru>
2024-05-16 19:17:28 +0300

    stage3/catalog/update написан сигнал для удаления кэша

--- src/shop/signals.py
+++ src/shop/signals.py
@@ -1,8 +1,7 @@
 from django.core.cache import cache
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
-from shop.models import Category, Product
+from shop.models import Category, Product, SellerProduct
 
 
 @receiver(signal=post_save, sender=Category)
@@ -14,3 +13,11 @@ def clear_menu_cache(sender, **kwargs):
 @receiver(post_save, sender=Product)
 def reset_cache(sender, instance, **kwargs):
     cache.delete('product_cache_key')
+
+
+@receiver(post_save, sender=SellerProduct)
+@receiver(post_delete, sender=SellerProduct)
+def clear_product_cache(sender, **kwargs):
+    keys = cache.keys('products-*')
+    for key in keys:
+        cache.delete(key)
