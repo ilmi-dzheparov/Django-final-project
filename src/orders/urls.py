bd015498913aae1dba0a72f8e2064d1e3b6f9059
@VladiFinogenov <Finogenov.V.A@yandex.ru>
2024-06-11 06:28:53 +0300

    Добавлены приложение orders в settings и urls для переходов

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -8,10 +8,9 @@ from .views import (
 
 app_name = "orders"
 
-
 urlpatterns = [
-    path("user-data/", Step1UserData.as_view(), name='user_data'),
-    path("select/delivery/", Step2SelectDelivery.as_view(), name='delivery'),
-    path("select/payment/", Step3SelectPayment.as_view(), name='payment'),
+    path("create/", Step1UserData.as_view(), name='order_create'),
+    path("select/delivery/", Step2SelectDelivery.as_view(), name='select_delivery'),
+    path("select/payment/", Step3SelectPayment.as_view(), name='select_payment'),
     path("confirmation/", Step4OrderConfirmation.as_view(), name='confirmation'),
 ]
