009af03d0264fa7e7957badcf4c3d66716953e7d
@VladiFinogenov <Finogenov.V.A@yandex.ru>
2024-06-11 06:29:13 +0300

    Добавлены приложение orders в settings и urls для переходов

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -30,6 +30,8 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
+class User(AbstractUser):
+
     phone_regex = RegexValidator(regex=r'^((\+7)|8)\d{10}$', message='Phone number must be entered in the format: "+79999999999" or "89999999999" ')
 
     email = models.EmailField(unique=True)
