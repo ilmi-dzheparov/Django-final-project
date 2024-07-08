ca6fdc4099cf40b0767d2562f7b50626c827c1d8
ilmi <jilmi@yandex.ru>
2024-07-08 10:39:54 +0100

    SP6_Интернализация части шаблонов

--- src/megano/settings.py
+++ src/megano/settings.py
@@ -180,6 +180,8 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOCALE_PATHS = [BASE_DIR / 'locale']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
