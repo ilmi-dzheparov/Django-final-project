4d1d4f73798ec55e5b02524f429d728971b675c5
ilmi <jilmi@yandex.ru>
2024-07-13 16:25:06 +0100

    SP6_Исправление представления и шаблонов со скидками 13.07.24

--- src/discounts/views.py
+++ src/discounts/views.py
@@ -1,13 +1,29 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
-from .models import BaseDiscount
+from .models import BaseDiscount, ProductDiscount, BundleDiscount, CartDiscount
 
 
-class DiscountListView(ListView):
-    model = BaseDiscount
+class DiscountListView(TemplateView):
     template_name = 'shop/discount.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_discount_list'] = ProductDiscount.objects.all()
+        context['bundle_discount_list'] = BundleDiscount.objects.all()
+        context['cart_discount_list'] = CartDiscount.objects.all()
+        return context
 
-class DiscountDetailView(DetailView):
-    model = BaseDiscount
-    template_name = 'discount_detail.html'
+
+class ProductDiscountDetailView(DetailView):
+    model = ProductDiscount
+    template_name = 'shop/discount_detail.html'
+
+
+class BundleDiscountDetailView(DetailView):
+    model = BundleDiscount
+    template_name = 'shop/discount_detail.html'
+
+
+class CartDiscountDetailView(DetailView):
+    model = CartDiscount
+    template_name = 'shop/discount_detail.html'
