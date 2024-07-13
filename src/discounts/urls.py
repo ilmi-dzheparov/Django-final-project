9f990e8f94aa49c0258d2305042488245e940dbe
ilmi <jilmi@yandex.ru>
2024-07-13 16:25:06 +0100

    SP6_Исправление представления и шаблонов со скидками 13.07.24

--- src/discounts/urls.py
+++ src/discounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import DiscountListView, DiscountDetailView
+from .views import DiscountListView, ProductDiscountDetailView, BundleDiscountDetailView, CartDiscountDetailView
 
 app_name = "discounts"
 
 urlpatterns = [
     path('discounts/', DiscountListView.as_view(), name='discount-list'),
-    path('discounts/int:pk/', DiscountDetailView.as_view(), name='discount-detail'),
+    path('discounts/product_discount/<int:pk>/', ProductDiscountDetailView.as_view(), name='product-discount-detail'),
+    path('discounts/bundle_discount/<int:pk>/', BundleDiscountDetailView.as_view(), name='bundle-discount-detail'),
+    path('discounts/cart_discount/<int:pk>/', CartDiscountDetailView.as_view(), name='cart-discount-detail'),
 ]
